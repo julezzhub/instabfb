6d46deccf4c5e76240a495fdc6a7bb853cec220d
Benjamin Wendelstadt <bwendelstadt@gmail.com>
2019-11-24 19:42:49 +0200

    changed schema (added columns to bf) and somewhat through homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
-  def home
+  def search
+    @boyfriends = Boyfriend.all
+    @search = params['search']
+    @location = @search['location']
+    @likes = @search['likes']
+    @height = @search['height']
+    @smartphone_model = @search['smartphone_model']
+    @pet = @search['pet']
+    @category = @search['category']
+
+    @boyfriends = Boyfriend.where(location: @location, likes: @likes, height: @height, smartphone_model: @smartphone_model, pet: @pet, category: @category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#search'
   resources :boyfriends, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:create]
     resources :bookings, only: [:create]
