afcd971a6fd418502720a3fc5679fe15f5440aee
Leopold von Schultzendorff <leopoldvonschultzendorff@gmail.com>
2019-11-24 16:08:37 +0200

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :boyfriends, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:show]
+  namespace :account do
+    resources :boyfriends, only: [:index]
+  end
 end
