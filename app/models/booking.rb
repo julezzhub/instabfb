5a864cc0589b47a901af7ba905a25a833d2909d7
Julius Selchow <julius_selchow@hotmail.com>
2019-11-25 13:11:40 +0200

    UPDATED: Booking Model, Booking Controller, Booking Views

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,12 @@ class Booking < ApplicationRecord
   belongs_to :boyfriend
 
   validates :start_date, presence: true
-  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates :end_date, presence: true
+  validate :check_date
+
+  private
+
+  def check_date
+    errors.add(:start_date, "Invalid: The Start-date must be before the End-date!") if start_date > end_date
+  end
 end
