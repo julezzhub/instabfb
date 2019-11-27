22375e40825c146e3bf986c9abc53b92f4f8811a
Leopold von Schultzendorff <leopoldvonschultzendorff@gmail.com>
2019-11-27 14:35:08 +0200

    geo search

--- app/controllers/boyfriends_controller.rb
+++ app/controllers/boyfriends_controller.rb
@@ -1,29 +1,32 @@
 class BoyfriendsController < ApplicationController
   def index
     @boyfriend = Boyfriend.new
-    if params.include?(:search) || params.include?(:boyfriend)
-      @search = params['search'] || params['boyfriend']
-      @location = @search['location'].capitalize
-      @likes = @search[:likes].split(";").map(&:to_i)
-      @likes_range = Boyfriend.range_likes(@likes[0], @likes[1])
+    @boyfriends = Boyfriend.geocoded #returns boyfriends with coordinates
+    @markers = @boyfriends.map do |boyfriend|
+      {
+        lat: boyfriend.latitude,
+        lng: boyfriend.longitude
+      }
+    end
+    if params.include?(:search) #|| params.include?(:boyfriend)
+      # @search = params['search'] || params['boyfriend']
+      @location_search = params['search']['location']
+      @message = "These Instaboys from #{@location_search} are ready to up your gram"
+      @boyfriends = Boyfriend.near(@location_search, 10)
+      # @likes = @search[:likes].split(";").map(&:to_i)
+      # @likes_range = Boyfriend.range_likes(@likes[0], @likes[1])
         # @height = @search['height']
         # @smartphone_model = @search['smartphone_model']
         # @pet = @search['pet']
         # @category = @search['category']
-      @boyfriends = @likes_range.select { |boyfriend| (boyfriend.location == @location) }
-      @message = "These Instaboys from #{@location} are ready to up your gram"
+      # @boyfriends = @likes_range.select { |boyfriend| (boyfriend.location == @location_search) }
       if @boyfriends.empty?
-        if @location.empty?
-          @message = 'Discover your next Instagram boyfriend'
-          @boyfriends = Boyfriend.all
-        else
-          @message = "No Instalovers meeting your standards. Browse some of our other options in #{@location}"
-          @boyfriends = Boyfriend.where(location: @location)
-        end
+        @message = "No Instalovers meeting your standards. Browse some of our other options below."
+        @boyfriends
       end
     else
       @message = 'Browse some our best Instagram boyfriends'
-      @boyfriends = Boyfriend.all
+      @boyfriends
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_174635) do
+ActiveRecord::Schema.define(version: 2019_11_27_093037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_11_24_174635) do
     t.string "smartphone_model"
     t.string "pet"
     t.string "category"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_boyfriends_on_user_id"
   end
 
