d7de644cb6fae209f8b30fb486803d43081060e4
Julius Hillebrand <hillebrandjulius@gmail.com>
2019-11-28 16:40:25 +0200

    last fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ puts "Destroying all users"
 addresses = ['32 Rothschild Boulevard, Tel Aviv', '70 Rothschild Boulevard, Tel Aviv', '10 Rothschild Boulevard, Tel Aviv', '32 Ben Yehuda, Tel Aviv', '10 Ben Yehuda, Tel Aviv', '100 Ben Yehuda, Tel Aviv', '1 Ben Yehuda, Tel Aviv', '32 Allenby, Tel Aviv', '1 Allenby, Tel Aviv','55 Allenby, Tel Aviv', 'Elbchaussee 1, Hamburg', 'Jungfernstieg 1, Hamburg', 'Reeperbahn 50, Hamburg', 'Hallerstrasse 10, Hamburg', 'Turmstrasse 2, Hamburg', 'Waitzstrasse 20, Hamburg', 'Neuer Jungfernstieg 20, Hamburg', 'Elbchausse 100, Hamburg', 'Neuer Wall 1, Hamburg', 'Neuer Wall 30, Hamburg']
 20.times do
   user = User.create(email: Faker::Internet.email, name: Faker::FunnyName.name, password: Faker::Alphanumeric.alpha(number: 10), remote_avatar_url: "https://source.unsplash.com/collection/566311/#{rand(1..268)}")
-  boyfriend = Boyfriend.create!(name: Faker::Name.male_first_name + " " + Faker::Name.last_name, description: Faker::Superhero.name,
+  boyfriend = Boyfriend.create!(name: Faker::Name.male_first_name + " " + Faker::Name.last_name, description: Faker::Quote.famous_last_words,
     likes: rand(10000..45000), location: addresses.sample, height: rand(160..210),
 
     smartphone_model: Faker::Device.model_name, pet: Faker::Creature::Animal.name, user: user, price: rand(50..2000))
