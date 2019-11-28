f32fe4c3f7fe49e2f3a522ed80fe2cb4ee45a019
Benjamin Wendelstadt <bwendelstadt@gmail.com>
2019-11-28 14:55:46 +0200

    pills to review

--- app/javascript/plugins/init_star_review.js
+++ app/javascript/plugins/init_star_review.js
@@ -1,9 +1,9 @@
 import "jquery-bar-rating";
-import "jquery-bar-rating/dist/themes/css-stars.css";
+// import "jquery-bar-rating/dist/themes/bars-pill.css";
 
 const initStarReview = () => {
   $('#review_rating').barrating({
-    theme: 'css-stars'
+    theme: 'bars-pill'
   });
 };
 
